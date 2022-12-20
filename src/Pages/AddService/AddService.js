9d898702c99a4b43fccd7c08d4bbb2ccef82a159
NazPro <najmulbinnurul3@gmail.com>
2022-12-20 22:47:21 +0600

    toast added

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
 const AddService = () => {
 
@@ -11,7 +12,18 @@ const AddService = () => {
         const description=form.description.value
 
         const service={img2, name, price, description}
-        console.log(service);
+        
+        fetch('https://roof-doctor-server-najmul11.vercel.app/service',{
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(service)
+        })
+        .then(res=>{})
+        .then(()=>{
+            toast.success('Service added successfully');
+        })
     }
     return (
         <div className='dark:bg-gray-800 min-h-screen'>
@@ -21,7 +33,7 @@ const AddService = () => {
                         <h2 className="card-title">Add service</h2>
                         <input type="text" placeholder="service name" name='name' className="input input-bordered w-full mt-2" />
                         <input type="text" placeholder="service image url" name='image' className="input input-bordered w-full mt-2" />
-                        <input type="number" placeholder="rating" name='price' className="input input-bordered w-full mt-2" />
+                        <input type="number" placeholder="price" name='price' className="input input-bordered w-full mt-2" />
                         <input type="text" placeholder="description" name='description' className="input input-bordered w-full mt-2" />
                         <div className="card-actions justify-end">
                         <button className="btn px-5 py-2 text-black dark:bg border-teal-400 mt-10 bg-transparent hover:bg-teal-50
@@ -30,6 +42,7 @@ const AddService = () => {
                     </form>
                 </div>  
             </div>
+            <Toaster />
         </div>
     );
 }; 
